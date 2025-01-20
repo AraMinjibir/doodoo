8c23539e4117a2d934e383e18018725c2a267f6d
AraMinjibir <abubakar.aramjb@gmail.com>
2025-01-20 10:35:46 +0100

    final submission

--- src/app/authmod/forgot-password/forgot-password.component.ts
+++ src/app/authmod/forgot-password/forgot-password.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 import { TuiAppearance, TuiButton, TuiTextfield} from '@taiga-ui/core';
 import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 import { TuiInputModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
@@ -18,10 +19,13 @@ import { TuiInputModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
-
-  protected readonly testForm = new FormGroup({
-      testValue: new FormControl(),
-      password: new FormControl(),
-      rememberMe: new FormControl(false)      
+  router: Router = inject(Router)
+  protected readonly forgotForm = new FormGroup({
+      email: new FormControl(),
+           
   });
+  onFormSubmitting(){
+    this.forgotForm.reset();
+    this.router.navigate(['auth/reset-password'])
+  }
 }
