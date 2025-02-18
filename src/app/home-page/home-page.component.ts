fe004d64fc84718e7f3d4f8dec66cf6bb386e66f
AraMinjibir <abubakar.aramjb@gmail.com>
2025-02-18 16:30:47 +0100

    initial

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,16 +1,22 @@
+import { NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { TuiButton } from '@taiga-ui/core';
+import { TuiTab, TuiTabs } from '@taiga-ui/kit';
 
 
 @Component({
   selector: 'home-page',
-  imports: [RouterLink],
+  imports: [RouterLink, TuiTabs, TuiButton, NgIf, TuiTab],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
 
   activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  activeTab = null; 
+  showTabs = false;
+
 
   ngOnInit(){
     this.activatedRoute.fragment.subscribe((data) =>{
@@ -20,4 +26,6 @@ export class HomePageComponent {
   jumpToView(section:any){
     document.getElementById(section)?.scrollIntoView({behavior:'smooth'})
   }
+
+ 
 }
