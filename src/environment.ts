388a7758725098f0d40ce5be251a59f38296ecd5
AraMinjibir <abubakar.aramjb@gmail.com>
2025-03-09 00:39:29 +0100

    environment added

---
+++ src/environment.ts
@@ -0,0 +1,15 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyA_TwU_V8GXFh0mMJBh8D5mmlc8zqQE_1o",
+      authDomain: "doodoo-cbfa9.firebaseapp.com",
+      databaseURL: "https://doodoo-cbfa9-default-rtdb.firebaseio.com",
+      projectId: "doodoo-cbfa9",
+      storageBucket: "doodoo-cbfa9.firebasestorage.app",
+      messagingSenderId: "343358445958",
+      appId: "1:343358445958:web:67216b931a413f2f6a7407",
+      measurementId: "G-VX0CN1MZ83",
+    },
+    
+  };
